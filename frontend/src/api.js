eca2f451438cdb3d11908da154471937d8e4284d
Sridhar_medishetti <53511528+Sridhar-medishetti@users.noreply.github.com>
2026-06-02 16:58:50 +0530

    Configure production backend URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://resume-analyser-zm57.onrender.com",
 });
 
 export default API;
